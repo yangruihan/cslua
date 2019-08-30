b10ed7dd00079090e35eeeb12c2ba824d5ee663a
C.y <yangruihan@vip.qq.com>
2019-08-30 11:12:57 +0800

    ch01

---
+++ CsLua/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+ 
+ namespace CsLua
+ {
+     internal class Program
+     {
+         public static void Main(string[] args)
+         {
+             Console.WriteLine("Hello, World!");
+         }
+     }
+ }
